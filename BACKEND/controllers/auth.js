f387139aeb62a4e367b97a9ada3a7a070ed0c18f
Chandimaml <it20104962@my.sliit.lk>
2021-08-21 13:37:11 +0530

    delivery second commit

--- BACKEND/controllers/auth.js
+++ BACKEND/controllers/auth.js
@@ -1,6 +1,7 @@
 //JWT authentication goes here
 const User = require("../models/user");
 const Staff = require("../models/staff");
+const deliveryStaff = require("../models/deliveryStaff");
 const ErrorResponse = require("../utils/errorResponse");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
@@ -326,7 +327,7 @@ exports.loginStafffoodM = async (req , res , next) =>{
     const {email , password} = req.body; //destructure method
 
     try {
-        const staff = await Staff.create({
+        const staff = await deliveryStaff.create({
             email , password //this.password filed of user.js in models
         })
         sendStaffToken(staff , 200 , res);
@@ -345,7 +346,7 @@ exports.loginStaffDeliveryM = async (req , res , next) =>{
  
     try {
      
-         const staff = await Staff.findOne({email}).select("+password");
+         const staff = await deliveryStaff.findOne({email}).select("+password");
  
          if(!staff){ //true
              return next(new ErrorResponse("Invalid Credentials" , 401));
