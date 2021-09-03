fc5b1152a4dcbdb5bfbaaa764c26202f6cb27ffa
ThamaliiNirmala <tnirmalaadasooriya@gmail.com>
2021-09-03 22:03:59 +0530

    Stock fourth commit

--- frontend/src/components/staff/stockM/shortcoming.js
+++ frontend/src/components/staff/stockM/shortcoming.js
@@ -39,7 +39,10 @@ export default function DisplayInventoryt() {
                 <Link className="nav-link" to = "/add-stockM"><i class="fa fa-user-circle" aria-hidden="true"></i> Add Inventory</Link>
             </li> 
             <li className="nav-item">
-                <Link className="nav-link active" to = "/display-stockM"><i class="fa fa-desktop" aria-hidden="true"></i> Display Inventory</Link>
+                <Link className="nav-link" to = "/display-stockM"><i class="fa fa-desktop" aria-hidden="true"></i> Display Inventory</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link active" to = "/display-stockM"><i class="fa fa-desktop" aria-hidden="true"></i> Display Shortcomings</Link>
             </li> 
           </ul>
         <form className="d-flex">
