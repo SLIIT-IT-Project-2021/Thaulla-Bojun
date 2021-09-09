6fe64630ed4a96d7f7093d1bbd1c7b47f062bcd0
NilakshiSandeepanie <86109418+NilakshiSandeepanie@users.noreply.github.com>
2021-09-10 01:10:38 +0530

    Update displayNeeds.js

--- BACKEND/routes/displayNeeds.js
+++ BACKEND/routes/displayNeeds.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 let path = require('path');
-let User = require('../models/supplier');
+let User = require('../models/request');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
